0eaf6422cb0062de006f5c0158f9a41beccafc63
Haskebutterfly <114040707+Haskebutterfly@users.noreply.github.com>
2022-10-08 11:28:24 +1100

    updated velocity graph

--- main/hasaan.m
+++ main/hasaan.m
@@ -1,25 +1,25 @@
 % This file is assigned to: Hasaan
 % This code does the plotting for the Velocity (Km/h) vs time (h)
 
-function output = hasaan(data)
+function velocity = hasaan(data1)
     % the code above creates the function for velocity, and gets the input values
-   disp("hi");
-   time = data(:, 1);
-   easting = data(:, 3);
-   northing = data(:, 2);
-   elevation = data(:, 4);
+
+   time = data1(:, 1);
+   easting = data1(:, 3);
+   northing = data1(:, 2);
+   elevation = data1(:, 4);
 
     t = time ./ (60 * 60);
     % the code above changes time from seconds to hours
 
     % the code above changes m into km
     
-    x_diff_shifted = shiftdim(easting, 1);
+    x_diff_shifted = circshift(easting,-1);
     x_diff = easting - x_diff_shifted;
     % the code above subtracts the second value of the array from the first
     % in which it creates an array with the difference in x distance
 
-    y_diff_shifted = shiftdim(northing, 1);
+    y_diff_shifted = circshift(northing,-1);
     y_diff = northing - y_diff_shifted;
     % the code above subtracts the second value of the array from the first
     % in which it creates an array with the difference in y distance
@@ -28,7 +28,7 @@ function output = hasaan(data)
     % the code above uses hypothesus rule to calculate the distance
     % between two points
 
-    ele_diff_shifted = shiftdim(elevation, 1);
+    ele_diff_shifted = circshift(elevation,-1);
     ele_diff = elevation - ele_diff_shifted;
     % the code above subtracts the second value of the array from the first
     % in which it creates an array with the difference in elevation
@@ -37,17 +37,17 @@ function output = hasaan(data)
     % the code above calculates the total distance between each point
     % including the elevation in calculation
 
-    output = total_dist ./ t;
+    velocity = total_dist ./ t;
     % the code above calculates the velocity
     % which is the distance divided by time
 
-    output(length(elevation)) = 0;
-    output = shiftdim(output, -1);
+    velocity(length(elevation)) = 0;
+    velocity = circshift(velocity, 1);
     % the code above makes the last value of velocity equal to 0
     % this is because shiftdim shifts the whole array
     % meaning that our first value got subtracted from the last value twice
 
-    plot(output, t), title('Plot of Velocity (Km/h) as function of time (h)'), xlabe('time (h)'), ylabel('velocity (km/h)');
-    % the code above plots the graph
+    plot(t, velocity), title('Plot of Velocity (Km/h) as function of time (h)'), xlabel('time (h)'), ylabel('velocity (km/h)');
+    %the code above plots the graph
     
 end
--- main/hasaan_test.m
+++ main/hasaan_test.m
@@ -1,15 +1,6 @@
 
 data = get_data_from_file('data_sample_1.gpx');
 
-function output = get_times(master_table)
-    output = master_table(:, 1);
-end
-function output = get_coords(master_table)
-    output = master_table(:, 2:3);
-end
-function output = get_elevations(master_table)
-    output = master_table(:, 4);
-end
-function output = hasaan(master_table)
-    output = master_table(:, 5);
-end
+velocity = hasaan(data);
+data(:,5) = velocity;
+
