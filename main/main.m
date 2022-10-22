888ef0dad22f841d1a78a7ac738ce415018c60fc
Angus Barnes <38348730+angusbarnes@users.noreply.github.com>
2022-10-22 18:09:08 +1100

    Further Clean Up

--- main/main.m
+++ main/main.m
@@ -1,63 +1,23 @@
-% This is the main high level submission file
-% We are allowed up to 10 LOC for this file
-% This ensures we split code effectively between
-% other external files
+% Add the folder containing helper functions to the MATLAB envrionement at
+% runtime
 addpath(genpath('helpers'))
-%filename = persistantDataPath + input('Data File Name');
-gpxParser = XMLParser('data/data_sample_1.gpx');
+
+% The data samples recorded by our group are located in the data
+% sub-directory. To access them you need to provide the full relative path
+% eg; data/data_sample_1.gpx
+% THIS MUST BE WRAPPED IN QUOTES WHEN INPUT TO TERMINAL
+filename = input('Relative Path To Data File: ');
+
+% THIS IS NOT A MATLAB BUILT IN
+% We wrote the XMLParser class from scratch using only two low level IO
+% functions. You can view this Parser Class in the file hierarchy at: 
+% ---> helpers/XMLParser.m 
+gpxParser = XMLParser(filename);
 results = gpxParser.Parse();
 
-% Ethan is sample 1?
+master_table = create_master_table(results);
 
-% Get time in seconds since hourly UTC Epoch
-% This would not handle rides that exceed 24 hours in length.
-% The first time is read from the metadata tag of the GPX file
-% This function returns all results as time elapsed in seconds since
-% that reference point.
-% Because of how GPS technologies tend to 'stabilise' as connected duration
-% increases, the initial time stamps may have repeated times
-master_table = zeros(results.dataCount, 6);
-
-times = results.filter('time', XMLParser.TIME_HANDLING_METHOD);
-
-% Get elevations in meters above see level
-% The generic NUMERICAL HANDLING METHOD will attempt to parse any number
-% stored inside the request tag name
-elevations = results.filter('ele', XMLParser.NUMERICAL_HANDLING_METHOD);
-
-coords = convert(results.filter('coords', XMLParser.COORDINATE_HANDLING_METHOD));
-
-master_table(:,1) = times;
-master_table(:,2:3) = coords;
-master_table(:,4) = elevations;
-
-previous_time = -1;
-output_table = zeros(size(master_table));
-insertIndex = 0;
-for row = 1:length(master_table)
-    time = master_table(row, 1);
-    
-         
-    if time > previous_time
-        insertIndex = insertIndex+1;
-        previous_time = time;
-    elseif time < previous_time
-        disp("There is a notable data consistency issue.")
-        disp("The provided file contains timestamps that go backwards in time")
-    end
-
-    output_table(insertIndex,:) = master_table(row, :);
-end
-
-% Truncate the the filtered table to remove trailing zeros made with
-% pre-allocation
-output_table = output_table(1:insertIndex, :);
-
-distances = interval_distance(output_table, 1);
-times = interval_time(output_table);
-vels = calculate_velocities(output_table);
-cums = cumsum(interval_distance(output_table, true));
-velocity_cumulative_distance(output_table);
+velocity_cumulative_distance(master_table);
 
 function save_plot(func, filename)
     func(); % Invoke the plotting function passed by the function handle 'func'
